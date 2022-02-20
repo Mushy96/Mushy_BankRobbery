c31834f6642d454f036bcd18791076c93a24f6c7
Mushy <97806278+Mushy96@users.noreply.github.com>
2022-02-20 00:24:48 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config = {}
+
+Config.rob = 'Press [~e~G~q~] to break into safe'
+
+--Timer and zone size
+Config.ZoneSize = 2.0 -- Sizes of the zones
+Config.LockpickTime = 5000 -- in miliseconds
+Config.Policealert = 5000 -- in ms
+Config.Cooldown = 120000 -- Cooldown beetwen the robberys
+
+Config.Items = {
+    "goldbar",
+    "diamond",
+}
+
+
+Config.Banks = {
+
+	{ coords = vector3(1290.0882568359, -1312.4019775391, 76.039939880371), name = "Rhodes Bank" },
+}
